3fd109e59e08c0b2f10a0c73ecb5a305479fb0d6
arthas168 <pavlovskipetko@abv.bg>
2020-03-05 17:57:32 +0200

    Started working on Providers.

--- src/components/providers/index.js
+++ src/components/providers/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Select from "react-select";
+import selectorStyles from "./selectorStyles";
 
 import "./styles.scss";
 
+const options = [
+  { value: "btc", label: "Bitcoin" },
+  { value: "eth", label: "Ethereum" },
+  { value: "vanilla", label: "Vanilla" }
+];
+
 export default () => {
-  return <span>Providers</span>;
+  return (
+    <div className="providers">
+      <div className="selector-wrapper">
+        <Select options={options} styles={selectorStyles()} />
+      </div>
+    </div>
+  );
 };
---
+++ src/components/providers/selectorStyles.js
@@ -0,0 +1,8 @@
+export default function styles() {
+  return {
+    container: provided => ({
+      ...provided,
+      width: "300px"
+    })
+  };
+}
