43927cea00b3ce8579f5c2561e631b223162b92b
arthas168 <pavlovskipetko@abv.bg>
2020-03-10 17:12:14 +0200

    Handling case where price of token is null (error from provider).

--- src/components/dashboard/Card.js
+++ src/components/dashboard/Card.js
@@ -2,10 +2,10 @@ import React from "react";
 import { formatDisplayNum } from "../../utils";
 
 export default ({ network, priceToUSD }) => {
-  return (
+  return priceToUSD ? (
     <div className="card">
       <span className="price">{formatDisplayNum(priceToUSD)} $</span>
       <span className="network">{network}</span>
     </div>
-  );
+  ) : null;
 };
